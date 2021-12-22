e24bcfcd7732976bd8ae9215c225154e0f9ea1a7
choisoobong <devcsb119@gmail.com>
2021-12-22 17:57:09 +0900

    add testcode public void save() public void findByName()

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemoryMemberRepositoryTest {
+
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();    //.get() --> optional에서 값을 꺼내는 메소드
+//        System.out.println("result = " + (result == member));    //일일히 print로 찍는 대신 assert를 사용하자.
+
+//        Assertions.assertEquals(result, member); // Assert that expected and actual are equal.If both are null, they are considered equal.
+        assertThat(member).isEqualTo(result);       // static import하여 Assertions 없이 사용.  위 코드와 같은 동작함.
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+}
