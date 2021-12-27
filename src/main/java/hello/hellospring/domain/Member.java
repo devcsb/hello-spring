136a37f7f9bdf24dc0df36d37c19b1312d27c7d8
devcsb <devcsb119@gmail.com>
2021-12-27 23:40:41 +0900

    add JpaMemberRepository - 90%

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,18 @@
 package hello.hellospring.domain;
 
-public class Member {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity // JPA가 관리하는 엔티티라고 선언.
+public class Member {
+    // DB가 id를 자동으로 생성해주는 것을 아이덴티티라고 부른다. 아이덴티티 전략.
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+//    @Column(name = "username") // 만약 db의 컬럼명이 username이라면 이렇게 어노테이션을 달아준다.
     private String name;
 
     public Long getId() {
